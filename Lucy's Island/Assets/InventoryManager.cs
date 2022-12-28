159e065d027120cc0c81d2d53670f33ec94394fa
Christopher <chrisriddle17@yahoo.com>
2022-12-27 22:41:50 -0600

    Added UI for inventory

--- Lucy's Island/Assets/InventoryManager.cs
+++ Lucy's Island/Assets/InventoryManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
-    public ItemClass itemToAdd;
-    public ItemClass itemToRemove;
+    [SerializeField] GameObject slotsHolder;
+    [SerializeField] ItemClass itemToAdd;
+    [SerializeField] ItemClass itemToRemove;
 
     public List<ItemClass> items = new List<ItemClass>();
 
-    private void Start()
+    GameObject[] slots;
+
+    void Start()
     {
+        slots = new GameObject[slotsHolder.transform.childCount];
+        //set all the slots
+        for (int i = 0; i < slotsHolder.transform.childCount; i++)
+        {
+            slots[i] = slotsHolder.transform.GetChild(i).gameObject;
+        }
+
+        RefreshUI();
         Add(itemToAdd);
         Remove(itemToRemove);
+    }
+
+    public void RefreshUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            try 
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = true;
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = items[i].itemIcon; 
+            }
+            catch 
+            { 
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null; 
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false; 
+            }
+        }
     }
     public void Add(ItemClass item)
     {
