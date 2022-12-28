c3bd4a4c0af67e2f729fbdfba408b5712d6fd9f0
Christopher <chrisriddle17@yahoo.com>
2022-12-27 22:02:42 -0600

    Started inventory

---
+++ Lucy's Island/Assets/ToolClass.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Tool Class", menuName = "Item/Tool")]
+public class ToolClass : ItemClass
+{
+    [Header("Tool")]
+    public ToolType toolType;
+    public enum ToolType
+    {
+        gun,
+        grenade,
+        knife,
+        sword,
+    }
+    public override ItemClass GetItem() { return this; }
+    public override ToolClass GetTool() { return this; }
+    public override MiscClass GetMisc() { return null; }
+    public override ConsumableClass GetConsumable() { return null; }
+}
